708e06f0d4cdc2912b56f625557e552b1ad58095
Zhenya Eremkin <john2196mail@yandex.ru>
2024-10-11 17:32:00 +0300

    brands

---
+++ brands/js/script.js
@@ -0,0 +1,26 @@
+let brandsBtn = document.getElementById('brands-btn');
+let brandsList = document.querySelector('.brands__list');
+let readMoreText = document.querySelector('.read-more__text');
+
+brandsBtn.addEventListener('mousedown', () => {
+    brandsList.classList.toggle('open');
+    brandsBtn.classList.toggle('arr');
+
+    if (readMoreText.textContent === 'Показать все') {
+        readMoreText.textContent = 'Скрыть';
+      } else {
+        readMoreText.textContent = 'Показать все';
+    }
+});
+
+const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  slidesPerView: 1,
+  width: 240,
+  spaceBetween: 16,
+  loop: false,
+
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});
